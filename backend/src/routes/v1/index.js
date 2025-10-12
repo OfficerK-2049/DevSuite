ad764119190c8a5e6ab5d4c58d5df0db260b984b
Tony Montana <ashwin.s1336@gmail.com>
2025-10-12 21:53:30 +0530

    feat : added base time endpoint

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import urlShortenerRoutes from './urlShortener.routes.js'
 import textTransformRoutes from './textTransform.routes.js'
+import timeZoneRoutes from './timeZone.routes.js'
 
 const router=express.Router()
 
@@ -8,5 +9,6 @@ router.use('/url',urlShortenerRoutes)
 
 router.use('/text',textTransformRoutes)
 
+router.use('/time',timeZoneRoutes)
 
 export default router

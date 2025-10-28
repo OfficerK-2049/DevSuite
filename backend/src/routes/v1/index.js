74f69299788c66c2b54bf343989f6d159be16a98
Jesse Pinkman <threeleafcloveraiv@gmail.com>
2025-10-28 17:53:03 +0530

    feat : create /cron route

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import urlShortenerRoutes from './urlShortener.routes.js'
 import textTransformRoutes from './textTransform.routes.js'
 import timeZoneRoutes from './timeZone.routes.js'
+import cronRoutes from './cronGenerator.routes.js'
 
 const router=express.Router()
 
@@ -10,5 +11,7 @@ router.use('/url',urlShortenerRoutes)
 router.use('/text',textTransformRoutes)
 
 router.use('/time',timeZoneRoutes)
+
+router.use('/cron', cronRoutes)
 
 export default router

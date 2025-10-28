daa0443dbe221d430d18997753fd783fc55dc1c5
Tony Montana <ashwin.s1336@gmail.com>
2025-10-28 20:30:44 +0530

    chore : "We are not ramping down . We're just getting started"

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,7 +45,7 @@ app.get('/health',(req,res)=>
 {
     res.json({
         status:'OK',
-        timestamp: new Date.toISOString()
+        timestamp: new Date.toTimeString()
     })
 })
 
---
+++ backend/src/services/cronGenerator.service.js
@@ -0,0 +1,1 @@
+/*let's stir up the pot */
